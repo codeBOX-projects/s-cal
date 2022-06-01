cd3be5820a83f67a3d5cd3c7990ba20bc4596072
codeBOX-projects <andrei.abd.codebox@gmail.com>
2022-06-01 12:35:27 +0300

    [code]Box

---
+++ README.md
@@ -0,0 +1,105 @@
+# **S-Cal**
+
+>**Introduction :**
+
+simple calculator program built in python,it can do 7 difference arithmetic operators with two numbers
+
+---
+
+> **Install :**
+
+***First intall requirements:***
+
+```pip install -r requirements.txt```
+
+***Run help list :***
+
+``` python3 s_cal.py -h ```
+
+***Or :***
+
+```python3 s_cal.py --help```
+
+***output:***
+
+```\
+█▀ ▄▄ █▀▀ ▄▀█ █░░
+▄█ ░░ █▄▄ █▀█ █▄▄
+
+[code]Box - Andrei A. Abd 2022
+
+[*] Introduction:
+    simple calculator built in python,it can do 7 difference arithmetic operators with two numbers.
+
+[*] Source: https://github.com/codeBOX-projects
+
+[*] Usage Examples: [First Number] [Arthimetic Operator] [Secon Number]
+
+    > Addition       : 100  +  50
+    > Subtraction    : 100  -  50
+    > Multiplication : 100  *  50
+    > Division       : 100  /  50
+    > Modulus        : 100  %  50
+    > Exponentiation : 100  ** 50
+    > Floor division : 100  // 50
+                                 
+
+positional arguments:
+  num1        First Number.
+  math_value  Arithmetic Operators.
+  num2        Second Number.
+
+options:
+  -h, --help  show this help message and exit
+```
+
+---
+>**Usage & Examples :**
+
+**[First Number]** **[Arthimetic Operator]** **[Secon Number]**
+
+## Examples
+
+- ### **Addition :**
+
+  ```python3 s_cal.py 100  "+"  50```
+
+- ### **Subtraction :**
+
+  ```python3 s_cal.py 100  "-"  50```
+
+- ### **Multiplication :**
+
+  ```python3 s_cal.py 100  "*"  50```
+
+- ### **Division :**
+
+  ```python3 s_cal.py 100  "/"  50```
+
+- ### **Modulus :**
+
+  ```python3 s_cal.py 100  "%"  50```
+
+- ### **Exponentiation :**
+
+  ```python3 s_cal.py 100  "**" 50```
+
+- ### **Floor division :**
+
+  ```python3 s_cal.py 100  "//"  50```
+
+### **Output :**
+
+```codeBOX@usr:~/python3 s_cal.py 100  "+"  50```
+
+```codeBOX@usr:~/[+] Resulte: 150```
+
+---
+
+> **Support distrebution :**
+
+- **Linux os.**
+- **Termux.**
+
+---
+> ***[code]BOX*** | ***Andrei A. Abd*** | ***1993 - 2022***
--- s_cal.py
+++ s_cal.py
@@ -2,7 +2,29 @@
 import argparse, termcolor
 from glob import escape
 
-parser = argparse.ArgumentParser(description="Example: [first number] [arithmetic operators] [second number] ")
+parser = argparse.ArgumentParser(description="""
+█▀ ▄▄ █▀▀ ▄▀█ █░░
+▄█ ░░ █▄▄ █▀█ █▄▄
+
+[code]Box - Andrei A. Abd 2022
+
+[*] Introduction:
+    simple calculator built in python,it can do 7 difference arithmetic operators with two numbers.
+
+[*] Source: https://github.com/codeBOX-projects
+
+[*] Usage Examples: [First Number] [Arthimetic Operator] [Secon Number]
+
+    > Addition       : 100  "+"  50
+    > Subtraction    : 100  "-"  50
+    > Multiplication : 100  "*"  50
+    > Division       : 100  "/"  50
+    > Modulus        : 100  "%"  50
+    > Exponentiation : 100  "**" 50
+    > Floor division : 100  "//" 50
+                                 """,
+                                 usage=termcolor.colored("python3 [first number] [arthmetic operator] [second number]",'red'),
+                                 formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('num1', type=int, help="First Number.")
 parser.add_argument('math_value', type=str, help="Arithmetic Operators.")
 parser.add_argument('num2', type=int, help="Second Number.")
@@ -11,25 +33,25 @@ args = parser.parse_args()
 def math_resulte(num1, math_value, num2):
     if math_value == "+":
         math_value = num1 + num2
-        print(termcolor.colored(math_value, 'green'))
+        print("[+] Resulte: " + termcolor.colored(math_value, 'green'))
     elif math_value == "-":
         math_value = num1 - num2
-        print(termcolor.colored(math_value, 'green'))
+        print("[-] Resulte: " + termcolor.colored(math_value, 'green'))
     elif math_value == "*":
         math_value = num1 * num2
-        print(termcolor.colored(math_value, 'green'))
+        print("[*] Resulte: " + termcolor.colored(math_value, 'green'))
     elif math_value == "/":
         math_value = num1 / num2
-        print(termcolor.colored(math_value, 'green'))
+        print("[/] Resulte: " + termcolor.colored(math_value, 'green'))
     elif math_value == "%":
         math_value = num1 % num2
-        print(termcolor.colored(math_value, 'green'))
+        print("[%] Resulte: " + termcolor.colored(math_value, 'green'))
     elif math_value == "**":
         math_value = num1 ** num2
-        print(termcolor.colored(math_value, 'green'))
+        print("[**] Resulte: " + termcolor.colored(math_value, 'green'))
     elif math_value == "//":
         math_value = num1 // num2
-        print(termcolor.colored(math_value, 'green'))
+        print("[//] Resulte: " + termcolor.colored(math_value, 'green'))
     else:
         print(termcolor.colored("[!] Error: This mathematical operation is not available",'red'))
     
